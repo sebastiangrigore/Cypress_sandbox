c74670fcc23984c5d2470e7cdc6e4490fa28fb47
Sebastian Grigore <sebastian.grigore@yahoo.com>
2021-04-09 10:00:51 +0300

    changes2

--- cypress/integration/examples/window.spec.js
+++ cypress/integration/examples/window.spec.js
@@ -16,7 +16,7 @@ context('Window', () => {
   })
 
   it('cy.title() - get the title', () => {
-    // https://on.cypress.io/titledasdasda
+    // https://on.cypress.io/title
     cy.title().should('include', 'Kitchen Sink')
   })
 })
